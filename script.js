0e670cdddcff3beba613621f872dc64f90a18e9b
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-04-09 16:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
+    if (num <= 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 }
 
-module.exports = fibonacci;
+// Test cases
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
